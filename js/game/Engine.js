e2a563fbabc0e3e72d5b8c97902cdb8c3b53249e
Michael Henretty <michael.henretty@gmail.com>
2020-02-13 13:11:41 -0800

    use orthometric perspective

--- js/game/Engine.js
+++ js/game/Engine.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 
 const ASPECT_RATIO = window.innerWidth / window.innerHeight;
-const SCREEN_WIDTH = 90;
+const SCREEN_WIDTH = 100;
 const SCREEN_HEIGHT = SCREEN_WIDTH / ASPECT_RATIO;
 const BOX_SIZE = 50;
 
@@ -10,11 +10,9 @@ export default class Engine {
   constructor() {
     this.scene = new THREE.Scene();
 
-    const ratio = SCREEN_WIDTH / SCREEN_HEIGHT;
-    this.camera = new THREE.PerspectiveCamera( 75, ratio, 0.1, 100000 );
-    this.camera.position.set( 400, 300, 1000 );
-    this.camera.lookAt( 0, 0, 1000 );
-    this.camera.position.z = 4;
+    this.camera = new THREE.PerspectiveCamera( 70, ASPECT_RATIO, 0.8, 1600 );
+    this.camera.position.set( 130, 400, 300 );
+    this.camera.lookAt( 20, -100, -150 );
 
     var geometry = new THREE.BoxGeometry(BOX_SIZE, BOX_SIZE, BOX_SIZE);
     for ( var i = 0; i < geometry.faces.length; i+=2 ) {
@@ -27,6 +25,7 @@ export default class Engine {
       vertexColors: THREE.FaceColors,
     });
     this.cube = new THREE.Mesh( geometry, material );
+    this.cube.position.set(0, BOX_SIZE / 2, 0);
     this.scene.add( this.cube );
 
     /*
@@ -34,24 +33,8 @@ export default class Engine {
     const intensity = 1;
     const light = new THREE.DirectionalLight(color, intensity);
     light.position.set(-1, 2, 4);
-    scene.add(light);
+    this.scene.add(light);
     */
-
-    this.animate = this.animate.bind(this);
-  }
-
-  animate() {
-    this.cube.rotation.z += 0.04;
-    this.cube.position.setX(this.cube.position.getComponent(0) - 3);
-    this.camera.position.setX(this.camera.position.getComponent(0) - 3);
-    requestAnimationFrame( this.animate );
-    this.renderer.render( this.scene, this.camera );
-  }
-
-  start() {
-    this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setPixelRatio(window.devicePixelRatio);
-    this.renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 
     var gridHelper = new THREE.GridHelper( 100000, 2001 );
     this.scene.add( gridHelper );
@@ -59,8 +42,26 @@ export default class Engine {
     var ambientLight = new THREE.AmbientLight( 0x606060 );
     this.scene.add( ambientLight );
 
-    const scale = Math.max(window.innerWidth / SCREEN_WIDTH, window.innerHeight / SCREEN_HEIGHT);
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 
+    this.animate = this.animate.bind(this);
+  }
+
+  animate() {
+    this.cube.rotation.x -= 0.04;
+    this.cube.position.setZ(this.cube.position.getComponent(2) - 3);
+    this.camera.position.setZ(this.camera.position.getComponent(2) - 3);
+    requestAnimationFrame( this.animate );
+    this.renderer.render( this.scene, this.camera );
+  }
+
+  start() {
+    const scale = Math.max(
+      window.innerWidth / SCREEN_WIDTH,
+      window.innerHeight / SCREEN_HEIGHT
+    );
     document.body.appendChild(this.renderer.domElement);
     this.renderer.domElement.style.transform = `scale(${scale})`;
 
