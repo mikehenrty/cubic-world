9390871fc7fdc4c120fac5b6d051107ae7d2c72b
Michael Henretty <michael.henretty@gmail.com>
2020-02-28 21:21:27 -0800

    tweak game board config

--- js/game/constants.js
+++ js/game/constants.js
@@ -3,10 +3,10 @@ import * as THREE from 'three';
 export const DEBUG = false;
 
 // Number of pixels in the screen.
-export const PIXEL_WIDTH = 120;
+export const PIXEL_WIDTH = 200;
 
 // Board constants.
-export const BOARD_DEPTH = 11;
+export const BOARD_DEPTH = 101;
 export const BOARD_WIDTH = 5
 
 // Cube constants.
