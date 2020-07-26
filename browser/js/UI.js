638d4d6918ee26a7f283bed958ce7ddca315e98d
Michael Henretty <michael.henretty@gmail.com>
2020-07-26 11:56:56 -0700

    allow starting game from overlay

--- browser/js/UI.js
+++ browser/js/UI.js
@@ -1,13 +1,31 @@
+export const EVT_START = 'start';
 export default class UI extends EventTarget {
   constructor() {
     super();
-
-    this.el = document.createElement('div');
-    this.el.id = 'UI';
+    this.createDOM();
   }
 
-  start() {
+  createDOM() {
+    this.el = document.createElement('div');
+    this.el.id = 'UI';
+
+    this.messageEl = document.createElement('p');
+    this.startButton = document.createElement('button');
+    this.startButton.textContent = 'Start Game';
+
+    this.startButton.addEventListener('click', this.onStart.bind(this));
+
+    this.el.appendChild(this.messageEl);
+    this.el.appendChild(this.startButton);
+  }
+
+  init() {
     document.body.appendChild(this.el);
+  }
+
+  onStart() {
+    this.hide();
+    this.dispatchEvent(new Event(EVT_START));
   }
 
   hide() {
@@ -19,10 +37,10 @@ export default class UI extends EventTarget {
   }
 
   setPeerList(list) {
-    this.el.textContent = list.toString();
+    this.messageEl.textContent = list.toString();
   }
 
   setErrorMsg(msg) {
-    this.el.textContent = msg;
+    this.messageEll.textContent = msg;
   }
 }
--- browser/js/index.js
+++ browser/js/index.js
@@ -1,6 +1,6 @@
 import Engine from './game/Engine';
 import Network, { EVT_PEERS } from './Network';
-import UI from './UI';
+import UI, { EVT_START } from './UI';
 
 
 const engine = new Engine();
@@ -8,22 +8,20 @@ const network = new Network();
 const ui = new UI();
 
 network.addEventListener(EVT_PEERS, (evt) => {
-  console.log('engine, got evt', evt);
   ui.setPeerList(evt.detail);
 });
 
-ui.addEventListener(UI.EVT_START, (evt) => {
+ui.addEventListener(EVT_START, (evt) => {
   engine.start();
 });
 
 async function run() {
   try {
-    ui.start();
+    ui.init();
     await network.start();
   } catch(e) {
     ui.setErrorMsg('ERROR: could not connect');
     console.error('could not connect', e);
-  } finally {
   }
 }
 
